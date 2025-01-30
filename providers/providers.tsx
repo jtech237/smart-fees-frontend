dd871a18a1caef11af4d41a6732412aed6cf8247
jtech237 <guttierez237@gmail.com>
2025-01-30 16:13:48 +0100

    Ajout de la prise en charge de React Query pour la gestion des requêtes asynchrones,  et refactorisation du composant AppProviders pour intégrer le QueryClientProvider.

--- providers/providers.tsx
+++ providers/providers.tsx
@@ -1,21 +1,27 @@
-"use client"
+"use client";
 
-import type {ThemeProviderProps} from "next-themes"
+import type { ThemeProviderProps } from "next-themes";
 
-import React from "react"
-import{ ThemeProvider as NextThemesProvider
-} from "next-themes"
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider } from 'next-auth/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import React, { useState } from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export interface AppProvidersProps {
   children: React.ReactNode;
-  themeProps?: ThemeProviderProps
+  themeProps?: ThemeProviderProps;
 }
 
-export function AppProviders({children, themeProps}: AppProvidersProps){
-  return (<NextThemesProvider {...themeProps}>
-    <SessionProvider>
-      {children}
-    </SessionProvider>
-  </NextThemesProvider>)
+export function AppProviders({ children, themeProps }: AppProvidersProps) {
+  const [queryClient] = useState(() => new QueryClient())
+  return (
+    <NextThemesProvider {...themeProps}>
+      <SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </SessionProvider>
+    </NextThemesProvider>
+  );
 }
